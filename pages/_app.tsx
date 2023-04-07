b915ebcd49d04cac1aecc5d91b62049c7561c683
Sumit Saxena <sumit.2019@tecorb.co>
2023-04-07 11:16:37 +0530

    Change structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import {store} from "../redux/store/store"
+import {store} from "../redux/Store/Store"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,35 +1,32 @@
 import Navbar from "../../component/Navbar/index";
-import {  useDispatch } from "react-redux";
-import { remove } from "../../redux/Slice/CartSlice"
+import { useDispatch } from "react-redux";
+import { remove } from "../../redux/Slice/CartSlice";
 import { useAppSelector } from "@/redux/store/hooks";
 
-
 export default function Cart() {
-    const dispatch = useDispatch()
-    const products = useAppSelector(state => state.cart)
+  const dispatch = useDispatch();
+  const products = useAppSelector((state) => state.cart);
 
-    const handleRemove = (_id: any) => {
-        dispatch(remove(_id))
-    }
+  const handleRemove = (_id: any) => {
+    dispatch(remove(_id));
+  };
 
-    return (
-        <div>
-            <Navbar />
-            <h3>Cart</h3>
+  return (
+    <div>
+      <Navbar />
+      <h3>Cart</h3>
+      <div>
+        {products?.map((product: any) => {
+          return (
             <div>
-                {
-                    products?.map((product: any) => {
-                        return (
-                            <div>
-                                <img src={product.image} alt="" />
-                                <h5>{product.title}</h5>
-                                <h5>{product.price}</h5>
-                                <button onClick={() => handleRemove(product?.id)}>Remove</button>
-                            </div>
-                        )
-                    })
-                }
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+              <h5>{product.price}</h5>
+              <button onClick={() => handleRemove(product?.id)}>Remove</button>
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
