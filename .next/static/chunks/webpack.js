70e82c0902548361bcc1912e2afb67a3ecf2e911
aman kumar <aman.22@tecorb.co>
2023-04-11 19:16:36 +0530

    product details design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd4e1fcf208e9615"; }
+/******/ 		__webpack_require__.h = function() { return "9a8c0698747c13e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
